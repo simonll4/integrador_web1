99971e6a93ce6c1e7d0aa1d8eba88d2f4fa81020
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2022-04-24 20:08:48 -0300

    starting proyect 1

--- ingweb/Integrador/Colegio/Ar/edu/aui/front/Sort.java
+++
@@ -1,5 +0,0 @@
-package ingweb.Integrador.Colegio.Ar.edu.aui.front;
-
-public class Sort {
-    
-}
--- tp_integrador-20220424T220536Z-001/tp_integrador/ar/edu/iua/negocio/academico/plan/BuscarPlan.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.iua.negocio.academico.plan;
-
-import ar.edu.iua.modelo.academico.plan.Plan;
-
-public interface BuscarPlan {
-
-    Plan buscar(int anio);
-    
-}
